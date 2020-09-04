003358946c3e31fad77cabf3d69f6b9857f8adf6
Vivyanne-504 <66225228+Vivyanne-504@users.noreply.github.com>
2020-09-04 23:21:16 +0100

    update

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,21 @@
-const APIURL= 'https://www.metaweather.com/api/';
-async function getWeatherByLocation() {
-  const resp = await fetch(APIURL+'/location/search/?query='+location);
-const respData = await resp.json();
+window.addEventListener('load', ()=> {
+ let long;
+ let lat;
+  if(navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(position =>{
+  long= position.coords.longitude;
+  lat= position.coords.latitude;
 
-
-
+  const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745bB36767721ad1/${lat},${long}`;
+  fetch(api)
+  .then(response =>{
+    return response.json();
+    })
+    .then(data =>{
+      console.log(data);
+    });
+  });
+  
 }
-getWeatherByLocation('Lagos')
+});
 
